84c5bca9b516a44050ccd649e87203e4a17031e1
Jan Pommerening <jan@mailpark.org>
2019-11-17 00:11:46 +0100

    end of day commit

---
+++ view/src/components/FruitForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+     <h1>test</h1>
+     <input :value="file" placeholder="add a image" type="file" @change="onFileChange">
+     <div>{{answer}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FruitForm extends Vue {
+   import axios from 'axios';
+
+   file: File;
+
+   answer: string;
+
+   constructor() {
+      super();
+      this.file = undefined;
+      this.answer = '';
+   }
+
+   private onFileChange() {
+     let response;
+     await response = axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+     this.answer = response;
+   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
