59bdefaa7760d3bacfaec75e5af460d0c3bda478
Jan Pommerening <jan@mailpark.org>
2019-11-16 16:54:26 +0100

    fix flask api

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,14 +1,11 @@
-import os
-
 from flask import Flask, request
 from keras_preprocessing.image import load_img
-
 from services.kerasModelService import loadKerasModel, checkImageFile
 import json
 
-from services.FileService import FileService
+
+
 app = Flask(__name__)
-fileService = FileService('temp')
 model = loadKerasModel('fruitModel')
 
 @app.route('/', methods=['GET'])
@@ -20,7 +17,7 @@ def getResult():
     if 'file' not in request.files:
         print('No file part')
     f = request.files['file']
-    result = checkImageFile(model, load_img(f, target_size = (64, 64)))
+    result = checkImageFile(model, load_img(f, target_size = (128, 128)))
     print(result)
     return result
 
