5b7cf29a1d0b1d02b209de90f5deeb514652b921
Jan Pommerening <jan@mailpark.org>
2019-11-17 09:31:42 +0100

    Revert "end of day commit"
    This reverts commit 84c5bca9b516a44050ccd649e87203e4a17031e1.

--- view/src/views/Home.vue
+++ view/src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to the UnnamedFruitScanProject"/>
-    <FruitForm file=""/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-import FruitForm from '@/components/FruitForm.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
-    FruitForm,
   },
 };
 </script>
